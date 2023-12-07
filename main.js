39f5ce3e4b9abf19e28f09214ad0f17cf64be508
Henrique Sabatini <henriquesabatini2016@gmail.com>
2023-12-07 19:37:15 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,67 +1,40 @@
-const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./images/aprovado.png" alt="emoji celebrando" />'; //imagem para retorno feliz ou trite (Aprovado ou reprovado 
-const imgReprovado = '<img src="./images/reprovado.png" alt="emoji triste" />'; //imagem para retorno feliz ou trite (Aprovado ou reprovado 
-const atividades = [];
-const notas = [];
-const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-const notaMinima = parseFloat(prompt("Digite a nota minima: ")) //pergunta ao usuario a nota minima para ser aprovado
-
-
+const form = document.getElementById('form-agenda');
+const nome = [];
+const numero = [];
     
-let linhas = ''; //somar notas e fazer a media
+let contatos = '';
 
 form.addEventListener('submit', function(e) { //modificando o enviar do formulario
     e.preventDefault(); //nao deixa a pagina atualizar quando clicar em submit
 
     adicionaLinha();
     atualizaTabela();
-    atualizaMediaFinal();
 });
 
 function adicionaLinha(){
-    const inputNomeAtividade = document.getElementById('nome-atividade'); //ativar compo para receber o dado
-    const inputNotaAtividade = document.getElementById('nota-atividade'); //ativar compo para receber o dado
+    const inputNome = document.getElementById('nome-contato'); //ativar compo para receber o dado
+    const inputNumero = document.getElementById('numero-contato'); //ativar compo para receber o dado
 
-    if (atividades.includes(inputNomeAtividade.value)){ //aqui criamos um if para o usuaruio nao colocar nome de atividade duplicado
-        alert(`A atividade: ${inputNomeAtividade.value} ja foi inserida`);
+    if (numero.includes(inputNumero.value)){ 
+        alert(`O numero: ${inputNumero.value} ja existe`);
     } else {
 
-        atividades.push(inputNomeAtividade.value);
-        notas.push(parseFloat(inputNotaAtividade.value)); //parseFloat para a nota inserida ser entendida como numero e nao string e usamos o parseFloat tambem pois o numero pode ser quebrado
+        nome.push(inputNome.value);
+        numero.push(parseFloat(inputNumero.value)); //parseFloat para a nota inserida ser entendida como numero e nao string e usamos o parseFloat tambem pois o numero pode ser quebrado
 
         let linha = '<tr>';
-        linha += `<td>${inputNomeAtividade.value}</td>`; //campo que vai receber o nome
-        linha += `<td>${inputNotaAtividade.value}</td>`; //campo que vai receber a nota
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`; //if e else para dizer se o aluno reprovou ou nao (alternaria (? If) e (: else))
+        linha += `<td>${inputNome.value}</td>`; //campo que vai receber o nome
+        linha += `<td>${inputNumero.value}</td>`;
         linha += `</tr>`;
-
-        linhas += linha; //somar notas e fazer a media
+        contatos += linha;
 
     }
 
-    inputNomeAtividade.value = ''; //depois que enviar o campo fica vazio de novo
-    inputNotaAtividade.value = ''; //depois que enviar o campo fica vazio de novo
+    inputNome.value = ''; //depois que enviar o campo fica vazio de novo
+    inputNumero.value = ''; //depois que enviar o campo fica vazio de novo
 }
 
 function atualizaTabela (){
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas; // usamos para inserir o conteudo acima dentro da tag (innerHTML)
-}
-
-function atualizaMediaFinal(){
-    const mediaFinal = calculaMediaFinal();  
-    
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
-}
-
-function calculaMediaFinal(){
-    let somaDasNotas = 0;
-
-    for(let i = 0; i< notas.length; i++) { //aqui fizemos um laço para que enquanto o usuario colocar nota (notas.length) ele vai se somando
-        somaDasNotas += notas[i];
-    }
-
-    return somaDasNotas / notas.length;
+    corpoTabela.innerHTML = contatos; // usamos para inserir o conteudo acima dentro da tag (innerHTML)
 }
